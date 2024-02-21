26d736039a19c7d7b6fa0723eea660a5612742ed
benno <benno@DESKTOP-84EU65G>
2024-02-21 23:15:17 +0100

    test1

---
+++ Ecommerce/src/main/java/com/Ecommerce/Ecommerce/EcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.Ecommerce.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableJpaRepositories(basePackages="com.Ecommerce.Ecommerce")
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ Ecommerce/src/main/java/com/Ecommerce/Ecommerce/Test.java
@@ -0,0 +1,10 @@
+package com.Ecommerce.Ecommerce;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ee");
+	}
+
+}
